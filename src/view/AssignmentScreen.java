304bee223aa9de6a3e00a2b42de096fc499e131a
prosiyashrestha <prosiyashrestha@gmail.com>
2022-12-11 15:33:47 +0545

    minor changes

--- src/view/AssignmentScreen.java
+++ src/view/AssignmentScreen.java
@@ -248,7 +248,7 @@ ResultSet rs;
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-    
+        
         
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton5ActionPerformed
